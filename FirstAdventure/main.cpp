c28bda7610e4146595602997c486769f5b1447ee
David Paris <davidparis@davids-mbp.mynet>
2018-01-23 22:41:35 +0000

    Add game description and some TODOs

--- FirstAdventure/main.cpp
+++ FirstAdventure/main.cpp
@@ -6,6 +6,7 @@
 //  Copyright © 2018 David Paris. All rights reserved.
 //
 
+
 #include <iostream>
 #include <string>
 #include "Player.hpp"
@@ -14,6 +15,15 @@
 
 using namespace std;
 
+
+/*
+ *
+ *  Game Description:
+ *
+ *  You find yourself in a dungeon.  You must search for a key to escape, then battle a Dragon before you are free.
+ *  Along the way you might pick up a sword and a shield.  If you do, it will make the battle with the dragon easier.
+ *
+ */
 int main(int argc, const char * argv[]) {
     
     //set up
@@ -30,13 +40,16 @@ int main(int argc, const char * argv[]) {
     
     while(true)
     {
+        //TODO: this is rendering
         cout << "You are in " << player.getRoom()->getDescription() << endl;
         cout << "There is an exit to the " << player.getRoom()->getExit()->getDirection() << endl;
         cout << "Where will you go?" << endl;
         
+        //TODO: this is handling input
         std::string direction;
         cin >> direction;
         
+        //TODO: this is updating the game
         if (direction.compare(player.getRoom()->getExit()->getDirection()) == 0)
         {
             player.setRoom(player.getRoom()->getExit()->getExitRoom());
